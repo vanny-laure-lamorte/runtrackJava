6d4ac4549d299fbc6340e378b6f5b436d65d9ea9
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-07-03 14:17:43 +0200

    jour8, job00

---
+++ jour8/joob00/AbstractFinal.java
@@ -0,0 +1,39 @@
+public class AbstractFinal {
+    public static void main(String[] args) {
+
+    // A a = new A(); // On ne peut pas instancier une classe abstraite
+    B b = new B();
+   // C c = new C(); // On ne peut pas instancier une classe abstraite
+    D d = new D();
+    d.m2();
+}
+}
+
+abstract class A {
+abstract void m1();
+
+final void m2() {
+    System.out.println("m2 in A");
+}
+}
+
+class B extends A {
+void m1() {
+    System.out.println("m1 in B");
+}
+}
+
+abstract class C extends B {
+abstract void m3();
+}
+
+final class D extends C {
+void m1() {
+    System.out.println("m1 in D");
+}
+
+void m3() {
+    System.out.println("m3 in D");
+}
+
+}
